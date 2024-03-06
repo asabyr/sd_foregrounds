a897c5d7e69cb1b4e2c273b0476593fbfa55312a
asabyr <76526893+asabyr@users.noreply.github.com>
2024-03-06 13:41:50 -0500

    Update spectral_distortions.py

--- spectral_distortions.py
+++ spectral_distortions.py
@@ -8,6 +8,7 @@ kboltz = 1.380649e-23  # MKS
 clight=299792458.0 #MKS
 m_elec = 510.999
 jy = 1.e26
+dT_factor=2.0*(kboltz*TCMB)**3.0/(hplanck*clight)**2.0*jy
 
 ndp = np.float64
 
@@ -16,6 +17,10 @@ this_dir=os.path.dirname(os.path.abspath(__file__))
 def DeltaI_DeltaT(freqs, DeltaT_amp=1.2e-4*TCMB):
     X = hplanck*freqs/(kboltz*TCMB)
     return (DeltaT_amp/TCMB * X**4.0 * np.exp(X)/(np.exp(X) - 1.0)**2.0 * 2.0*(kboltz*TCMB)**3.0 / (hplanck*clight)**2.0 * jy).astype(ndp)
+
+def DeltaI_DeltaT_old(freqs, DeltaT_amp=1.2e-4):
+    X = hplanck*freqs/(kboltz*TCMB)
+    return (DeltaT_amp * X**4.0 * np.exp(X)/(np.exp(X) - 1.0)**2.0 * 2.0*(kboltz*TCMB)**3.0 / (hplanck*clight)**2.0 * jy).astype(ndp)
 
 def DeltaI_mu(freqs, mu_amp=2.e-8):
     X = hplanck*freqs/(kboltz*TCMB)
@@ -53,7 +58,42 @@ def recombination(freqs, scale=1.0):
     return scale * 10.0**template(np.log10(freqs))
 
 ###################################### additions ######################################
-def DeltaI_reltSZ(freqs, y_tot, kT_yweight):
+def DeltaI_reltSZ_w1(freqs, y_tot, kT_yweight, omega):
+    Yorder=3
+    #based on Abitbol+2017, Hill+2015, uses Y functions of Nozawa+2006, Itoh+1998
+    yIGM_plus_yreion=1.87e-7
+    X=hplanck*freqs/(kboltz*TCMB)
+    Xtwid=X*np.cosh(0.5*X)/np.sinh(0.5*X)
+    Stwid=X/np.sinh(0.5*X)
+
+    #Y functions
+    Y0=Xtwid-4.0
+    Y1=-10.0+23.5*Xtwid-8.4*Xtwid**2+0.7*Xtwid**3+Stwid**2*(-4.2+1.4*Xtwid)
+    Y2=-7.5+127.875*Xtwid-173.6*Xtwid**2.0+65.8*Xtwid**3.0-8.8*Xtwid**4.0+11.0/30.0*Xtwid**5.0\
+    +Stwid**2.0*(-86.8+131.6*Xtwid-48.4*Xtwid**2.0+143.0/30.0*Xtwid**3.0)\
+    +Stwid**4.0*(-8.8+187.0/60.0*Xtwid)
+
+    Y3=7.5+313.125*Xtwid-1419.6*Xtwid**2.0+1425.3*Xtwid**3.0-18594.0/35.0*Xtwid**4.0+12059.0/140.0*Xtwid**5.0-128.0/21.0*Xtwid**6.0+16.0/105.0*Xtwid**7.0\
+    +Stwid**2.0*(-709.8+2850.6*Xtwid-102267.0/35.0*Xtwid**2.0+156767.0/140.0*Xtwid**3.0-1216.0/7.0*Xtwid**4.0+64.0/7.0*Xtwid**5.0)\
+    +Stwid**4.0*(-18594.0/35.0+205003.0/280.0*Xtwid-1920.0/7.0*Xtwid**2.0+1024.0/35.0*Xtwid**3.0)\
+    +Stwid**6.0*(-544.0/21.0+992.0/105.0*Xtwid)
+    #gfuncrel=Y0+Y1*(kT_yweight/const.m_elec)+Y2*(kT_yweight/const.m_elec)**2.0+Y3*(kT_yweight/const.m_elec)**3.0+Y4*(kT_yweight/const.m_elec)**4.0
+    #add different y orders
+    orders=np.array([Y0,Y1,Y2,Y3])
+    gfuncrel=0.0
+    for i in range(Yorder+1):
+        gfuncrel+=orders[i]*(kT_yweight/m_elec)**i
+        # print(f"added {i}")
+    if Yorder==0:
+        gfuncrel=Y0
+
+    Trelapprox = (yIGM_plus_yreion * Y0 + (y_tot-yIGM_plus_yreion) * (gfuncrel+(Y2*(kT_yweight/m_elec)**2.0+3*Y3*(kT_yweight/m_elec)**3.0)*omega)) * (TCMB*1e6)
+    Planckian = X**4.0*np.exp(X)/(np.exp(X) - 1.0)**2.0
+    DeltaIrelapprox = Planckian*Trelapprox / (TCMB*1e6)
+
+    return DeltaIrelapprox
+
+def DeltaI_reltSZ(freqs, y_tot=1.77e-6, kT_yweight=1.245):
     Yorder=4
     #based on Abitbol+2017, Hill+2015, uses Y functions of Nozawa+2006, Itoh+1998
     yIGM_plus_yreion=1.87e-7
@@ -96,5 +136,98 @@ def DeltaI_reltSZ(freqs, y_tot, kT_yweight):
     Planckian = X**4.0*np.exp(X)/(np.exp(X) - 1.0)**2.0
     DeltaIrelapprox = Planckian*Trelapprox / (TCMB*1e6)
 
-    return DeltaIrelapprox
+    return DeltaIrelapprox*dT_factor
+
+def DeltaI_reltSZ_Y3(freqs, y_tot, kT_yweight):
+    Yorder=3
+    #based on Abitbol+2017, Hill+2015, uses Y functions of Nozawa+2006, Itoh+1998
+    yIGM_plus_yreion=1.87e-7
+    X=hplanck*freqs/(kboltz*TCMB)
+    Xtwid=X*np.cosh(0.5*X)/np.sinh(0.5*X)
+    Stwid=X/np.sinh(0.5*X)
+
+    #Y functions
+    Y0=Xtwid-4.0
+    Y1=-10.0+23.5*Xtwid-8.4*Xtwid**2+0.7*Xtwid**3+Stwid**2*(-4.2+1.4*Xtwid)
+    Y2=-7.5+127.875*Xtwid-173.6*Xtwid**2.0+65.8*Xtwid**3.0-8.8*Xtwid**4.0+11.0/30.0*Xtwid**5.0\
+    +Stwid**2.0*(-86.8+131.6*Xtwid-48.4*Xtwid**2.0+143.0/30.0*Xtwid**3.0)\
+    +Stwid**4.0*(-8.8+187.0/60.0*Xtwid)
+
+    Y3=7.5+313.125*Xtwid-1419.6*Xtwid**2.0+1425.3*Xtwid**3.0-18594.0/35.0*Xtwid**4.0+12059.0/140.0*Xtwid**5.0-128.0/21.0*Xtwid**6.0+16.0/105.0*Xtwid**7.0\
+    +Stwid**2.0*(-709.8+2850.6*Xtwid-102267.0/35.0*Xtwid**2.0+156767.0/140.0*Xtwid**3.0-1216.0/7.0*Xtwid**4.0+64.0/7.0*Xtwid**5.0)\
+    +Stwid**4.0*(-18594.0/35.0+205003.0/280.0*Xtwid-1920.0/7.0*Xtwid**2.0+1024.0/35.0*Xtwid**3.0)\
+    +Stwid**6.0*(-544.0/21.0+992.0/105.0*Xtwid)
+
+    #gfuncrel=Y0+Y1*(kT_yweight/const.m_elec)+Y2*(kT_yweight/const.m_elec)**2.0+Y3*(kT_yweight/const.m_elec)**3.0+Y4*(kT_yweight/const.m_elec)**4.0
+    #add different y orders
+    orders=np.array([Y0,Y1,Y2,Y3])
+    gfuncrel=0.0
+    for i in range(Yorder+1):
+        gfuncrel+=orders[i]*(kT_yweight/m_elec)**i
+        # print(f"added {i}")
+    if Yorder==0:
+        gfuncrel=Y0
+
+    Trelapprox = (yIGM_plus_yreion * Y0 + (y_tot-yIGM_plus_yreion) * gfuncrel) * (TCMB*1e6)
+    Planckian = X**4.0*np.exp(X)/(np.exp(X) - 1.0)**2.0
+    DeltaIrelapprox = Planckian*Trelapprox / (TCMB*1e6)
+
+    return DeltaIrelapprox*dT_factor
+
+def DeltaI_reltSZ_Y2(freqs, y_tot, kT_yweight):
+    Yorder=2
+    #based on Abitbol+2017, Hill+2015, uses Y functions of Nozawa+2006, Itoh+1998
+    yIGM_plus_yreion=1.87e-7
+    X=hplanck*freqs/(kboltz*TCMB)
+    Xtwid=X*np.cosh(0.5*X)/np.sinh(0.5*X)
+    Stwid=X/np.sinh(0.5*X)
+
+    #Y functions
+    Y0=Xtwid-4.0
+    Y1=-10.0+23.5*Xtwid-8.4*Xtwid**2+0.7*Xtwid**3+Stwid**2*(-4.2+1.4*Xtwid)
+    Y2=-7.5+127.875*Xtwid-173.6*Xtwid**2.0+65.8*Xtwid**3.0-8.8*Xtwid**4.0+11.0/30.0*Xtwid**5.0\
+    +Stwid**2.0*(-86.8+131.6*Xtwid-48.4*Xtwid**2.0+143.0/30.0*Xtwid**3.0)\
+    +Stwid**4.0*(-8.8+187.0/60.0*Xtwid)
+    #gfuncrel=Y0+Y1*(kT_yweight/const.m_elec)+Y2*(kT_yweight/const.m_elec)**2.0+Y3*(kT_yweight/const.m_elec)**3.0+Y4*(kT_yweight/const.m_elec)**4.0
+    #add different y orders
+    orders=np.array([Y0,Y1,Y2])
+    gfuncrel=0.0
+    for i in range(Yorder+1):
+        gfuncrel+=orders[i]*(kT_yweight/m_elec)**i
+        # print(f"added {i}")
+    if Yorder==0:
+        gfuncrel=Y0
+
+    Trelapprox = (yIGM_plus_yreion * Y0 + (y_tot-yIGM_plus_yreion) * gfuncrel) * (TCMB*1e6)
+    Planckian = X**4.0*np.exp(X)/(np.exp(X) - 1.0)**2.0
+    DeltaIrelapprox = Planckian*Trelapprox / (TCMB*1e6)
+
+    return DeltaIrelapprox*dT_factor
+
+def DeltaI_reltSZ_Y1(freqs, y_tot, kT_yweight):
+    Yorder=1
+    #based on Abitbol+2017, Hill+2015, uses Y functions of Nozawa+2006, Itoh+1998
+    yIGM_plus_yreion=1.87e-7
+    X=hplanck*freqs/(kboltz*TCMB)
+    Xtwid=X*np.cosh(0.5*X)/np.sinh(0.5*X)
+    Stwid=X/np.sinh(0.5*X)
+
+    #Y functions
+    Y0=Xtwid-4.0
+    Y1=-10.0+23.5*Xtwid-8.4*Xtwid**2+0.7*Xtwid**3+Stwid**2*(-4.2+1.4*Xtwid)
+    #gfuncrel=Y0+Y1*(kT_yweight/const.m_elec)+Y2*(kT_yweight/const.m_elec)**2.0+Y3*(kT_yweight/const.m_elec)**3.0+Y4*(kT_yweight/const.m_elec)**4.0
+    #add different y orders
+    orders=np.array([Y0,Y1])
+    gfuncrel=0.0
+    for i in range(Yorder+1):
+        gfuncrel+=orders[i]*(kT_yweight/m_elec)**i
+        # print(f"added {i}")
+    if Yorder==0:
+        gfuncrel=Y0
+
+    Trelapprox = (yIGM_plus_yreion * Y0 + (y_tot-yIGM_plus_yreion) * gfuncrel) * (TCMB*1e6)
+    Planckian = X**4.0*np.exp(X)/(np.exp(X) - 1.0)**2.0
+    DeltaIrelapprox = Planckian*Trelapprox / (TCMB*1e6)
+
+    return DeltaIrelapprox*dT_factor
 ###################################### additions ######################################
